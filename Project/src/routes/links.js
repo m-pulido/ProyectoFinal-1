1d13d96648d5731d32c727e06bc2fd7c1bdf8484
Martin <mgl.0589@gmail.com>
2020-01-23 19:29:30 -0600

    Modificacion de regsitro de llamadas

--- Project/src/routes/links.js
+++ Project/src/routes/links.js
@@ -53,7 +53,7 @@ router.post('/addbenef', async (req, res) => {
     res.redirect('/links/listbenef');
 });
 
-// Render the list total of beneficiaries in database
+// Render the total of beneficiaries in database
 router.get('/listbenef', async (req, res) => {
     const beneficiarios = await pool.query('SELECT * FROM beneficiarios');
     /* console.log(beneficiarios); */ // Validates values received
@@ -64,18 +64,65 @@ router.get('/listbenef', async (req, res) => {
 router.get('/addcalls/:ID_BENEFICIARIO', async (req, res) => {
     const { ID_BENEFICIARIO } = req.params;
     /* console.log(ID_BENEFICIARIO); */ // Validates the id received
-    await pool.query('INSERT INTO registro_llamadas (ID_BENEFICIARIO) VALUES(?)', [ID_BENEFICIARIO]); // Register IDEBENEFICIARIO into registro_llamadas table
-    const newLinkCalls = await pool.query('SELECT * FROM registro_llamadas WHERE ID_BENEFICIARIO = ?', ID_BENEFICIARIO);
+    //await pool.query('INSERT INTO registro_llamadas (ID_BENEFICIARIO) VALUES(?)', [ID_BENEFICIARIO]); // Register IDEBENEFICIARIO into registro_llamadas table
+    const newLinkCalls = await pool.query('SELECT * FROM registro_llamadas WHERE ID_BENEFICIARIO = ? ORDER BY ID_LLAMADA DESC LIMIT 1', ID_BENEFICIARIO);
     const newLinkBenef = await pool.query('SELECT * FROM beneficiarios WHERE ID_BENEFICIARIO = ?', [ID_BENEFICIARIO]);
     /* console.log(newLinkBenef[0], newLinkCalls[0]); */ // Validates values from both querys
     res.render('links/addcalls', {newLinksBenef: newLinkBenef[0], newLinksCalls: newLinkCalls[0]});
 });
 
-// Receive the new data to add new phone calls
-/* router.post('/addcalls', async (req, res) => {
+// Receive the new data to add new phone calls and data of beneficiaries
+router.post('/addcalls/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const { 
+        CURP,
+        NOMBRE,
+        APELLIDO_PATERNO,
+        APELLIDO_MATERNO,
+        TEL_CASA,
+        TEL_CELULAR,
+        CORREO,
+        PROGRAMA,
+        CALLE,
+        NUM_EXT,
+        NUM_INT,
+        COLONIA,
+        CODIGO_POSTAL,
+        SEXO,
+        RESULTADO_LLAMADA,
+        CONFIRMACION,
+    } = req.body;
 
-    res.send('received');
-}); */
+    const updateBenef = {
+        ID_BENEFICIARIO,
+        CURP,
+        NOMBRE,
+        APELLIDO_PATERNO,
+        APELLIDO_MATERNO,
+        TEL_CASA,
+        TEL_CELULAR,
+        CORREO,
+        PROGRAMA,
+        CALLE,
+        NUM_EXT,
+        NUM_INT,
+        COLONIA,
+        CODIGO_POSTAL,
+        SEXO
+    };
+
+    const newCall = {
+        ID_BENEFICIARIO,
+        RESULTADO_LLAMADA,
+        CONFIRMACION
+    };
+    /* console.log(ID_BENEFICIARIO, updateBenef, newCall); */ // Validates values received
+    await pool.query('UPDATE beneficiarios set ? WHERE ID_BENEFICIARIO = ?', [updateBenef, ID_BENEFICIARIO]);
+    await pool.query('INSERT INTO registro_llamadas set ?', [newCall]);
+    // await pool.query('UPDATE registro_llamadas SET RESULTADO_LLAMADA = ?, CONFIRMACION = ? WHERE ID_BENEFICIARIO = ? ORDER BY ID_LLAMADA DESC LIMIT 1', [updateCall.RESULTADO_LLAMADA, updateCall.CONFIRMACION, ID_BENEFICIARIO]);
+    /* console.log(ID_BENEFICIARIO, updateBenef, updateCall.RESULTADO_LLAMADA, updateCall.CONFIRMACION); */ // Validates values received for updating tables
+    res.redirect('/links/listbenef');
+});
 
 
 module.exports = router;
