ac1ba8e4069c90edc6a5a0d45239013d1b8357c0
Martin <mgl.0589@gmail.com>
2020-01-27 23:17:20 -0600

    Creacion de vistas para asistenicia y entrega

--- Project/src/routes/links.js
+++ Project/src/routes/links.js
@@ -90,7 +90,7 @@ router.post('/addcalls/:ID_BENEFICIARIO', async (req, res) => {
         CODIGO_POSTAL,
         SEXO,
         RESULTADO_LLAMADA,
-        CONFIRMACION,
+        CONFIRMACION
     } = req.body;
 
     const updateBenef = {
@@ -119,9 +119,166 @@ router.post('/addcalls/:ID_BENEFICIARIO', async (req, res) => {
     /* console.log(ID_BENEFICIARIO, updateBenef, newCall); */ // Validates values received
     await pool.query('UPDATE beneficiarios set ? WHERE ID_BENEFICIARIO = ?', [updateBenef, ID_BENEFICIARIO]);
     await pool.query('INSERT INTO registro_llamadas set ?', [newCall]);
-    // await pool.query('UPDATE registro_llamadas SET RESULTADO_LLAMADA = ?, CONFIRMACION = ? WHERE ID_BENEFICIARIO = ? ORDER BY ID_LLAMADA DESC LIMIT 1', [updateCall.RESULTADO_LLAMADA, updateCall.CONFIRMACION, ID_BENEFICIARIO]);
+    // await pool.query('UPDATE registro_llamadas SET RESULTADO_LLAMADA = ?, CONFIRMACION = ? WHERE ID_BENEFICIARIO = ? ORDER BY ID_LLAMADA DESC LIMIT 1', [updateCall.RESULTADO_LLAMADA, updateCall.CONFIRMACION, ID_BENEFICIARIO]); // delete
     /* console.log(ID_BENEFICIARIO, updateBenef, updateCall.RESULTADO_LLAMADA, updateCall.CONFIRMACION); */ // Validates values received for updating tables
     res.redirect('/links/listbenef');
+});
+
+// Render the form to add assistance of beneficiaries
+router.get('/searchbenef', (req, res) => {
+    res.render('links/searchbenef');
+});
+
+// Receive the data to initialize a search of beneficiaries
+router.post('/searchbenef', async (req, res) => {
+    try {
+        const {
+            CURP,
+            NOMBRE,
+            APELLIDO_PATERNO,
+            APELLIDO_MATERNO,
+            TEL_CASA,
+            TEL_CELULAR,
+            CORREO
+        } = req.body;
+    
+        /* const newSearchBenef = {
+            CURP,
+            NOMBRE,
+            APELLIDO_PATERNO,
+            APELLIDO_MATERNO,
+            TEL_CASA,
+            TEL_CELULAR,
+            CORREO
+        }; */
+        
+        /* console.log(newSearchBenef.CURP, newSearchBenef.NOMBRE); */ // Validates values received
+        if (CURP != '') {
+            const listBenef = await pool.query('SELECT * FROM beneficiarios WHERE CURP = ?', [CURP]);
+            res.render('links/searchbenef', {listBenef});
+        }
+
+        else if (TEL_CASA != '') {
+            const listBenef = await pool.query('SELECT * FROM beneficiarios WHERE TEL_CASA = ?', [TEL_CASA]);
+            res.render('links/searchbenef', {listBenef});
+        }
+
+        else if (TEL_CELULAR != '') {
+            const listBenef = await pool.query('SELECT * FROM beneficiarios WHERE TEL_CELULAR = ?', [TEL_CELULAR]);
+            res.render('links/searchbenef', {listBenef});
+        }
+
+        else if (CORREO != '') {
+            const listBenef = await pool.query('SELECT * FROM beneficiarios WHERE CORREO = ?', [CORREO]);
+            res.render('links/searchbenef', {listBenef});
+        }
+        
+        else if (NOMBRE != '' || APELLIDO_PATERNO != '' || APELLIDO_MATERNO != '') {
+            const listBenef = await pool.query('SELECT * FROM beneficiarios WHERE NOMBRE = ? OR APELLIDO_PATERNO = ? OR APELLIDO_MATERNO', [NOMBRE, APELLIDO_PATERNO, APELLIDO_MATERNO]);
+            res.render('links/searchbenef', {listBenef});
+        }
+    }
+    
+    catch(e) {
+        console.log(e);
+    }
+});
+
+// Render the form to search specific beneficiarie
+router.get('/modifbenef/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const newLinkBenef = await pool.query('SELECT * FROM beneficiarios WHERE ID_BENEFICIARIO = ?', [ID_BENEFICIARIO]);
+    /* console.log(newLinkBenef[0]); */ // Validates values from both querys
+    res.render('links/modifbenef', {newLinksBenef: newLinkBenef[0]});
+});
+
+// Receives the new data to update specific beneficiarie
+router.post('/modifbenef/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const {
+        CURP,
+        NOMBRE,
+        APELLIDO_PATERNO,
+        APELLIDO_MATERNO,
+        TEL_CASA,
+        TEL_CELULAR,
+        CORREO,
+        PROGRAMA,
+        CALLE,
+        NUM_EXT,
+        NUM_INT,
+        COLONIA,
+        CODIGO_POSTAL,
+        SEXO
+    } = req.body;
+
+    const updateBenef = {
+        ID_BENEFICIARIO,
+        CURP,
+        NOMBRE,
+        APELLIDO_PATERNO,
+        APELLIDO_MATERNO,
+        TEL_CASA,
+        TEL_CELULAR,
+        CORREO,
+        PROGRAMA,
+        CALLE,
+        NUM_EXT,
+        NUM_INT,
+        COLONIA,
+        CODIGO_POSTAL,
+        SEXO
+    };
+    /* console.log(updateBenef[0]); */ // Validates values received
+    await pool.query('UPDATE beneficiarios set ? WHERE ID_BENEFICIARIO = ?', [updateBenef, ID_BENEFICIARIO]);
+    await pool.query('INSERT INTO registro_asistencias (ID_BENEFICIARIO, ASISTENCIA) VALUES(?, "PRESENTE")', [ID_BENEFICIARIO]);
+    res.redirect('/links/searchbenef');
+});
+
+// Render the form to add assistances of beneficiaries
+router.get('/addassist/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const newLinkAssist = await pool.query('SELECT * FROM registro_asistencias WHERE ID_BENEFICIARIO = ? ORDER BY ID_ASISTENCIA DESC LIMIT 1', ID_BENEFICIARIO);
+    const newLinkBenef = await pool.query('SELECT * FROM beneficiarios WHERE ID_BENEFICIARIO = ?', [ID_BENEFICIARIO]);
+    /* console.log(newLinkBenef[0], newLinkAssist[0]); */ // Validates values from both querys
+    res.render('links/addassist', {newLinksBenef: newLinkBenef[0], newLinksAssist: newLinkAssist[0]});
+});
+
+// Receives the new data to add assistance of beneficiaries
+router.post('/addassist/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const { ASISTENCIA } = req.body;
+
+    const newAsistance = {
+        ID_BENEFICIARIO,
+        ASISTENCIA
+    };
+    /* console.log(ID_BENEFICIARIO, newAsistance); */ // Validates values received
+    await pool.query('INSERT INTO registro_asistencias set ?', [newAsistance]);
+    res.redirect('/links/searchbenef');
+});
+
+// Render the form to add deliveries of kits
+router.get('/adddelivery/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const newLinkDelivery = await pool.query('SELECT * FROM registro_entregas WHERE ID_BENEFICIARIO = ? ORDER BY ID_ENTREGA DESC LIMIT 1', ID_BENEFICIARIO);
+    const newLinkBenef = await pool.query('SELECT * FROM beneficiarios WHERE ID_BENEFICIARIO = ?', [ID_BENEFICIARIO]);
+    /* console.log(newLinkBenef[0], newLinkAssist[0]); */ // Validates values from both querys
+    res.render('links/adddelivery', {newLinksBenef: newLinkBenef[0], newLinksDelivery: newLinkDelivery[0]});
+});
+
+// Receives the new data to add a delivery
+router.post('/adddelivery/:ID_BENEFICIARIO', async (req, res) => {
+    const { ID_BENEFICIARIO } = req.params;
+    const { ESTATUS_ENTREGA } = req.body;
+
+    const newDelivery = {
+        ID_BENEFICIARIO,
+        ESTATUS_ENTREGA
+    };
+    console.log(newDelivery); // Validates values received
+    await pool.query('INSERT INTO registro_entregas set ?', [newDelivery]);
+    res.redirect('/links/searchbenef');
 });
 
 
