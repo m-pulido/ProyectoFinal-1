4124ff2aabf9f01e35abb0e2ec7ac6fcbf4b8694
Martin <mgl.0589@gmail.com>
2020-02-17 19:06:38 -0600

    Ajustes de diseño de vistas

--- Project/src/keys.js
+++ Project/src/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
 
     database: {
         host: 'localhost',
@@ -7,9 +7,9 @@ module.exports = {
         database: 'prueba1'
     }
 
-}
+} */
 
-/* module.exports = {
+module.exports = {
 
     database: {
         host: 'localhost',
@@ -17,6 +17,6 @@ module.exports = {
         password: '3l3ctroQuimica_2',
         database: 'prueba1'
     }
-} */
+}
 
 // Change the port according the database server (default MYSQL: 3306)
